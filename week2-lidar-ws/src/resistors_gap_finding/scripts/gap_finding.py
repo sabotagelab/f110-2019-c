f3ab5c130c109298b74831c03ac48c14d15ed366
Michaela Buchanan <buchanam@oregonstate.edu>
2019-04-16 10:27:07 -0700

    first attempt feeding data into kmeans, not sure if entirely correct yet

--- week2-lidar-ws/src/resistors_gap_finding/scripts/gap_finding.py
+++ week2-lidar-ws/src/resistors_gap_finding/scripts/gap_finding.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python
 
+import time
 import rospy
+from scipy.cluster.vq import vq, kmeans2, whiten
 from geometry_msgs.msg import Vector3
 from sensor_msgs.msg import LaserScan
+import numpy as np
 
 def kmeans(data):
-	return None
-	
+	whiten(data)
+	#random.seed((1000,2000))
+	codes = 3
+	results = kmeans2(data, 3, iter=2, minit='points')
+	print(results)	
+
 def publish(clusterData):
 	return None
  
 def callback(data):
-	for item in data.ranges:
-		rospy.loginfo(item)
+	# create numpy array from data
+	# format: [ range , intensity ]
+	dataArray = np.empty((1081,3), dtype='float')
+	numEntries = len(data.ranges)
+	
+	for x in range(0, numEntries):
+		tmp = np.array([x, data.ranges[x], data.intensities[x]], dtype='float')
+		#print(tmp)
+		dataArray[x][:] = tmp
+		#rospy.loginfo(data.ranges[x], data.intensities[x])
+	print(dataArray)
+	print("END")
+	kmeans(dataArray)
+	time.sleep(10)	
+	#for item in data.ranges:
+		#rospy.loginfo(item)
 	
 
 def listener():
@@ -27,7 +48,8 @@ def listener():
 
 	#define cycle rate
 #	rate = rospy.Rate(1) #1GHz currently
-	
+
+		
 	rospy.spin()		
 
 if __name__ == '__main__':
