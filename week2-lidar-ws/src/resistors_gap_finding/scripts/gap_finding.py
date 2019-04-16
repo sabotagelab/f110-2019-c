2c7ee8becee124b2e422a8d6e535430a1cbd3a28
Michaela Buchanan <buchanam@oregonstate.edu>
2019-04-16 13:26:39 -0700

    everything works except publishing

--- week2-lidar-ws/src/resistors_gap_finding/scripts/gap_finding.py
+++ week2-lidar-ws/src/resistors_gap_finding/scripts/gap_finding.py
@@ -6,16 +6,33 @@ from scipy.cluster.vq import vq, kmeans2, whiten
 from geometry_msgs.msg import Vector3
 from sensor_msgs.msg import LaserScan
 import numpy as np
+import math
 
 def kmeans(data):
 	whiten(data)
 	#random.seed((1000,2000))
 	codes = 3
 	results = kmeans2(data, 3, iter=2, minit='points')
-	print(results)	
+	print(results)
+	pickGap(results[0])
 
-def publish(clusterData):
-	return None
+def pickGap(results):
+	np.argsort(results, axis=0)
+	middleGap = results[1,:]
+	print("MIDDLE GAP: ")
+	print(middleGap)
+	polarToCartesian(middleGap)	
+
+def polarToCartesian(middleGap):
+	x = middleGap[1] * math.cos(0.004363 * middleGap[0])
+	y = middleGap[1] * math.sin(0.004363 * middleGap[0])
+	print("X: ")
+	print(x)
+	print("Y: ")
+	print(y)
+	gapCenter = [x, y, 0.0]
+	pub.publish(gapCenter)
+
  
 def callback(data):
 	# create numpy array from data
@@ -41,14 +58,15 @@ def listener():
 	rospy.init_node('find_gap', anonymous=True)	
 
 	#create publisher for topic lidar_gaps
-#	pub = rospy.Publisher('lidar_gaps', anonymous=True)
+	pub = rospy.Publisher('lidar_gaps', Vector3, queue_size=100)
 	
 	#subscribe to lidar data
 	rospy.Subscriber('scan', LaserScan, callback)	
-
+	
+	
 	#define cycle rate
 #	rate = rospy.Rate(1) #1GHz currently
-
+	#pub.publish(gapCenter)
 		
 	rospy.spin()		
 
--- week2-lidar-ws/src/resistors_gap_finding_org/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/michaela/f110-2019-c/week2-lidar-ws/src/resistors_gap_finding/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
