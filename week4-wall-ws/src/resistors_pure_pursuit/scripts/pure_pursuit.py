fd5eba8f08349054ec65a8fb19481b781aded259
rondjj <jesserond@gmail.com>
2019-05-30 12:21:52 -0700

    Pure Pursuit from Jesse

--- week4-wall-ws/src/resistors_pure_pursuit/scripts/pure_pursuit.py
+++ week4-wall-ws/src/resistors_pure_pursuit/scripts/pure_pursuit.py
@@ -8,7 +8,8 @@ import numpy as np
 from numpy import linalg as LA
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import csv
-import os 
+import os
+
 
 #############
 # CONSTANTS #
@@ -17,12 +18,16 @@ import os
 LOOKAHEAD_DISTANCE = 1.5 # meters
 VELOCITY = 0.5 # m/s
 
-
 ###########
 # GLOBALS #
 ###########
 
-# Import waypoints.csv into a list (path_points)
+global car_position, startup_flag
+car_position = [0,0]
+startup_flag = 1;       # Flag so that first time in callback, it searches ALL waypoints to find closest
+
+# Import waypoints.csv into a list
+# path_points are [x, y, theta]
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, '../waypoints/levine-waypoints.csv')
 with open(filename) as f:
@@ -33,6 +38,8 @@ path_points = [(float(point[0]), float(point[1]), float(point[2])) for point in 
         
 # Publisher for 'drive_parameters' (speed and steering angle)
 pub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
+
+
 
 
 #############
@@ -46,54 +53,117 @@ def dist(p1, p2):
 # Input data is PoseStamped message from topic /pf/viz/inferred_pose.
 # Runs pure pursuit and publishes velocity and steering angle.
 def callback(data):
+    global car_position, startup_flag, closest_index, shortest_dis, num_path_points
+    print('')
+    print('JUST GOT POSE UPDATE')
+
+    # Get Car's Current Position
+    car_position[0] = data.pose.position.x
+    car_position[1] = data.pose.position.y
+    print('Car Location: ' + str(car_position))
+
+
+    # If this is first time in callback, must search ALL waypoints to find closest
+    if startup_flag == 1:
+        startup_flag = 0;
+        num_path_points = len(path_points)   # How many waypoints are there to search?
+        shortest_dis = 50000;       # Arbitrarily Large
+        closest_index = num_path_points +10;   # Artibtrarily Large
+        for i in range(0,num_path_points):
+            dist_temp = dist(car_position,[path_points[i][0], path_points[i][1]])   # Find Car to Waypoint Distance
+            if dist_temp < shortest_dis:
+                # If you found a new shortest waypoint distance
+                shortest_dis = dist_temp;
+                closest_index = i;
+    
+    print('Shortest Point: ' + str(closest_index) + '   Shortest Dis: ' + str(shortest_dis))
+
 
     # Note: These following numbered steps below are taken from R. Craig Coulter's paper on pure pursuit.
 
     # 1. Determine the current location of the vehicle (we are subscribed to vesc/odom)
     # Hint: Read up on PoseStamped message type in ROS to determine how to extract x, y, and yaw.
-    
-	x_pos = data.pose.position.x
-        y_pos = data.pose.position.y
-        quaternion = (
-                data.pose.orientation.x,
-                data.pose.orientation.y,
-                data.pose.orientation.z,
-                data.pose.orientation.w)
-        euler = euler_from_quaternion(quaternion)
-        yaw = euler[2]       
+
+
+    quaternion = (
+    data.pose.orientation.x,
+    data.pose.orientation.y,
+    data.pose.orientation.z,
+    data.pose.orientation.w)
+
+    euler = euler_from_quaternion(quaternion)
+    # roll = euler[0]
+    # pitch = euler[1]
+    yaw = euler[2]
+
+    print('Yaw is: ' + str(yaw))
+
 
     # 2. Find the path point closest to the vehicle that is >= 1 lookahead distance from vehicle's current location.
+    # print('path_points: ' + str(path_points))
 
-	currentPoint = [x_pos, y_pos, yaw]
-	
-	minDistance = 10000
-	bestPoint = []
-	for point in path_points:
-		dist = dist(currentPoint, point)
-		if (dist < minDistance and dist >= LOOKAHEAD_DISTANCE):
-			minDistance = dist
-			bestPoint = point
+    closest_coordinates = [path_points[closest_index][0], path_points[closest_index][1]]
+    closest_dis = dist(car_position, closest_coordinates)
+
+    print('Closest Point: ' + str(closest_coordinates) + '   Closest Dis: ' + str(closest_dis))
+
+
+    # Find Waypoint that is Right at the LOOKAHEAD_DISTANCE
+    while closest_dis <= LOOKAHEAD_DISTANCE:
+        closest_index = closest_index + 1;     # If inside LOOKAHEAD, increment to next waypoint
+        if closest_index >= num_path_points:
+            closest_index = 0;        # Reset to 0 when you get to the last waypoint
+        closest_dis = dist(car_position, [path_points[closest_index][0], path_points[closest_index][1]])  # Find distance to that next waypoint
+    # closest_index = closest_index - 1;     # Current index is ouside of LOOKAHEAD_DISTANCE, so step one back
+
+    # Recalculate Coordinates and Distance with New Waypoint
+    closest_coordinates = [path_points[closest_index][0], path_points[closest_index][1]]
+    closest_dis = dist(car_position, closest_coordinates)
+    print('Waypoint at Lookahead Distance: Index(' + str(closest_index) + ')   Dis(' + str(closest_dis)+')')
+
+
 
     # 3. Transform the goal point to vehicle coordinates. 
-	# converting to car reference frame
-	new_x = bestPoint[0] - currentPoint[0]
-	new_y = bestPoint[1] - currentPoint[1]
-	new_yaw = tan(new_y / new_x)
+    x = car_position[0];
+    y = car_position[1];
+    a = path_points[closest_index][0];
+    b = path_points[closest_index][1];
+    num = b-y;
+    den = a-x;
+    print('b-y = ' + str(num))
+    print('a-x = ' + str(den))
 
-	target_point = [new_x, new_y, new_yaw]    
-   
+
+    # ERROR IN MANUAL: They mix up beta and gamma. I swapped the two in these equations.
+    gamma = np.arctan(num/den);
+    beta = (np.pi)/2 - yaw - gamma;
+    x_car2goal = closest_dis*np.cos(gamma)
+    y_car2goal = closest_dis*np.sin(gamma)
+
+    print('Car  X,Y: ' + str(x) + '   '+str(y))
+    print('Goal X,Y: ' + str(a) + '   '+str(b))
+    print('Beta: ' + str(beta))
+    print('Gamma: ' + str(gamma))
+    print('C2G  X,Y: ' + str(x_car2goal) + '   '+str(y_car2goal))
+
+
     # 4. Calculate the curvature = 1/r = 2x/l^2
     # The curvature is transformed into steering wheel angle by the vehicle on board controller.
     # Hint: You may need to flip to negative because for the VESC a right steering angle has a negative value.
-	# curvature = 2|y|/(LD^2) according to lecture slides
-	curvature = (2 * abs(target_point[1])) / (LOOKAHEAD_DISTANCE * LOOKAHEAD_DISTANCE)	
-    
-    	angle = np.clip(curvature, -0.4189, 0.4189) # 0.4189 radians = 24 degrees because car can only turn 24 degrees max
+    curvature = (2*x_car2goal)/(closest_dis*closest_dis);
+    angle = -curvature;
 
-   	msg = drive_param()
-    	msg.velocity = VELOCITY
-    	msg.angle = angle
-    	pub.publish(msg)
+
+
+
+    # print(data)
+    # angle = 0
+    angle = np.clip(angle, -0.4189, 0.4189) # 0.4189 radians = 24 degrees because car can only turn 24 degrees max
+
+    msg = drive_param()
+    msg.velocity = VELOCITY
+    msg.angle = angle
+    pub.publish(msg)
     
 if __name__ == '__main__':
     rospy.init_node('pure_pursuit')
